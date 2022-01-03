d743126145e716aca1b00193b4b893e56ed63ffe
vuong <nguyenvuong1122000@gmail.com>
2022-01-04 00:48:26 +0700

    fix eth

--- crypto/keys/ethsecp256k1/ethsecp256k1_nocgo.go
+++ crypto/keys/ethsecp256k1/ethsecp256k1_nocgo.go
@@ -4,6 +4,7 @@
 package ethsecp256k1
 
 import (
+	ethcrypto "github.com/ethereum/go-ethereum/crypto"
 	"math/big"
 
 	ethsecp256k1 "github.com/btcsuite/btcd/btcec"
@@ -32,6 +33,15 @@ func (privKey *PrivKey) Sign(msg []byte) ([]byte, error) {
 // VerifyBytes verifies a signature of the form R || S.
 // It rejects signatures which are not in lower-S form.
 func (pubKey *PubKey) VerifySignature(msg []byte, sigStr []byte) bool {
+	if len(sigStr) == ethcrypto.SignatureLength {
+		// remove recovery ID (V) if contained in the signature
+		sigStr = sigStr[:len(sigStr)-1]
+		// the signature needs to be in [R || S] format when provided to VerifySignature
+		if ethcrypto.VerifySignature(pubKey.Key, ethcrypto.Keccak256Hash(msg).Bytes(), sigStr) == true {
+			return true
+		}
+	}
+
 	if len(sigStr) != 64 {
 		return false
 	}
@@ -46,6 +56,7 @@ func (pubKey *PubKey) VerifySignature(msg []byte, sigStr []byte) bool {
 	if signature.S.Cmp(ethsecp256k1halfN) > 0 {
 		return false
 	}
+
 	return signature.Verify(crypto.Sha256(msg), pub)
 }
 
